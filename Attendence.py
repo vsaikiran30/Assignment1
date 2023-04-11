db780e92bce010484a44f27df3b00a65497acf19
vsaikiran30 <130428842+vsaikiran30@users.noreply.github.com>
2023-04-11 12:38:19 +0530

    Update Attendence.py

--- Attendence.py
+++ Attendence.py
@@ -1,6 +1,8 @@
 TotalClasses_held = int(input("Total Classes held: "))
 Classes_attended = int(input("Classes attended: ")) 
 attendence_percentage = (Classes_attended/TotalClasses_held)*100 
+if TotalClasses_held <= Classses_attended:
+    print("Attended classes are should not more than Held classes") 
 if attendence_percentage >= 75:
     print("Attendence_percentage:",attendence_percentage)
     print("Student is allowed to sit in exam")
